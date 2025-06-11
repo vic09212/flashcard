aa8ff585752aa8a4abcdd23213faaf9c327e41e0
vic09212 <victoriabiella9@gmail.com>
2025-06-11 07:39:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Vingadores',
+    'Qual o primeiro vingador?',
+    'O Capitão America'
+)
+
+criaCartao(
+    'Conhecimento geral',
+    'Qual o orgão responsavel por bombear sangue?',
+    'O coração'
+)
+
+criaCartao(
+    'alanzoka',
+    'Oque a abelha come?',
+    'A Florrrr'
+)
